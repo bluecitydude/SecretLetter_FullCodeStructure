b1da60bc5ecde90ce56e8482bbe8752fbb0b41d2
TejendraPurohit <167332577+bluecitydude@users.noreply.github.com>
2026-05-28 08:29:15 +0530

    Change alert message and redirect URL in index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php 
 
 
- echo "<script>alert('redirecting you to main website');
+ echo "<script>alert('No entry!!');
+ setTimeout(function() {
+    window.location.href = 'https://secretletter-livid.vercel.app/index.html';
+}, 3000);
 </script>";
 
 ?>
